085e3a5f75eb4c570af5621401ebbc46c0709b00
victorbalbinoaraujo <victorbalbinoaraujo1@outlook.com>
2021-12-09 17:34:46 -0300

    Código em Beta -> Testá-lo posteriormente

--- BabyFest/src/com/anna/babyfest/Funcionario.java
+++ BabyFest/src/com/anna/babyfest/Funcionario.java
@@ -1,63 +1,27 @@
 package com.anna.babyfest;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Funcionario {
     private int id;
     private String nome;
     private String setor;
     private double salario;
 
-    private List<Funcionario> exibirf = new ArrayList<>();
+    public void setId(int id) { this.id = id; }
+    public int getId() { return id; }
 
-    public List<Funcionario> getExibirf() {
-        return exibirf;
-    }
+    public void setNome(String nome) { this.nome = nome; }
+    public String getNome() { return nome; }
 
-    public void exibirLista(Funcionario exibirf){
-        Gerenciar.exibirf.exibirLista(exibirf);
-    }
+    public void setSetor(String setor) { this.setor = setor; }
+    public String getSetor() { return setor; }
 
-    public Funcionario(int id, String nome, String setor, double salario) {
-        this.id = id;
-        this.nome = nome;
-        this.setor = setor;
-        this.salario = salario;
-    }
+    public void setSalario(double salario) { this.salario = salario; }
+    public double getSalario() { return salario; }
 
-    public Funcionario() {
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getSetor() {
-        return setor;
-    }
-
-    public void setSetor(String setor) {
-        this.setor = setor;
-    }
-
-    public double getSalario() {
-        return salario;
-    }
-
-    public void setSalario(double salario) {
-        this.salario = salario;
+    public void imprimirFuncionario() {
+        System.out.println("ID do Funcionário: " + id);
+        System.out.println("Nome do Fucionário: " + nome);
+        System.out.println("Setor do Funcionário: " + setor);
+        System.out.println("Salário do Funcionário: " + salario + "\n");
     }
 }
--- BabyFest/src/com/anna/babyfest/Gerenciar.java
+++ BabyFest/src/com/anna/babyfest/Gerenciar.java
@@ -1,99 +1,144 @@
 package com.anna.babyfest;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
 public class Gerenciar {
-
-    public  static Funcionario exibirf;
-    private final Scanner input = new Scanner(System.in);
-    private final List<Funcionario> funcionarios = new ArrayList<>();
+    static int id = 1;
+    static Scanner input = new Scanner(System.in);
+    static List<Funcionario> funcionarios = new ArrayList<>();
 
     public static void main(String[] args) {
-        Gerenciar gerenciar = new Gerenciar();
-        exibirf = new Funcionario();
-        Scanner input = new Scanner(System.in);
-        int op = 0;
+        int option;
+
         do {
-            System.out.println("-----Funcionário----- ");
+            System.out.println("\n-----Funcionário-----\n");
             System.out.println("1 - Cadastrar funcionário");
             System.out.println("2 - Alterar funcionário");
             System.out.println("3 - Excluir funcionário");
             System.out.println("4 - Sair");
             System.out.println("Escolha uma das opções: ");
-            op = Integer.parseInt(input.nextLine());
-            switch (op) {
-                case 1:
-                    gerenciar.cadastrarFuncionario();
-                    break;
-                case 2:
-                    gerenciar.alterarFuncionario();
-                    break;
-                case 3:
-                    gerenciar.excluirFuncionario();
-                    break;
-                case 4:
-                    System.out.println("Saindo do Sistema...");
-                    break;
+            option = input.nextInt();
 
+            switch(option) {
+                case 1: cadastrarFuncionario();
+                    break;
+                case 2: alterarFuncionario();
+                    break;
+                case 3: excluirFuncionario();
+                    break;
+                case 4: sair();
+                    break;
                 default:
-                    System.out.println("Opção Inválida," +
-                            "Escolha outra opção! ");
+                    System.err.println("\nOpção inválida!\n");
+                    break;
             }
-        } while (op != 4);
+        } while(option != 4);
     }
 
-
-    Funcionario funcionario = new Funcionario();
-
-    public Funcionario getFuncionario() {
-        return funcionario;
-    }
-
-    public void setFuncionario(Funcionario funcionario) {
-        this.funcionario = funcionario;
-    }
-
-    public void cadastrarFuncionario() {
-        System.out.println("Digite o ID do funcionário:");
-        funcionario.setId(Integer.parseInt(input.nextLine()));
-        System.out.println("Digite o nome do funcionário:");
-        funcionario.setNome(input.nextLine());
-        System.out.println("Digite o setor do funcionário:");
-        funcionario.setSetor(input.nextLine());
-        System.out.println("Digite o salário do funcionário:");
-        funcionario.setSalario(Double.parseDouble(input.nextLine()));
-        funcionarios.add(funcionario);
-    }
-
-    public void alterarFuncionario() {
-        Funcionario funcionarioproc = new Funcionario();
-        for (int i = 1; i < funcionarios.size(); i++) {
-            System.out.println("LISTA DE funcionarios  \n" + funcionarios);
+    public static void preencherDados(Funcionario f) {
+        System.out.println("Por favor, digite o nome do Funcionário: ");
+        if (input.hasNextLine()) {
+            String nome = input.nextLine();
+            f.setNome(nome);
         }
+        String nome = input.nextLine();
+        f.setNome(nome);
 
-        if(funcionarios.contains(funcionarioproc)) {
-           funcionarioproc.equals(funcionario);
-           excluirFuncionario();
+        System.out.println("Agora, em qual setor " + f.getNome() + " trabalhará?");
+        String setor = input.nextLine();
+        f.setSetor(setor);
+
+        System.out.println("E quanto " + f.getNome() + " ganhará por mês?");
+        try {
+            if (input.hasNextLine()) {
+                double salario = input.nextDouble();
+                f.setSalario(salario);
+            }
+        } catch (InputMismatchException e) {
+            System.err.println("\nSalário inválido! Por favor, digite apenas números!\n");
+            input.nextLine();
+            main(null);
         }
+    }
 
-        System.out.println("Alterando dados");
-        System.out.println("Digite o ID do funcionário:");
-        funcionario.setId(Integer.parseInt(input.nextLine()));
-        System.out.println("Digite o nome do funcionário:");
-        funcionario.setNome(input.nextLine());
-        System.out.println("Digite o setor do funcionário:");
-        funcionario.setSetor(input.nextLine());
-        System.out.println("Digite o salário do funcionário:");
-        funcionario.setSalario(Double.parseDouble(input.nextLine()));
-        funcionarios.add(funcionario);
+    public static void listaFuncionarios() {
+        System.out.println("\nLista de Funcionários\n");
+        for (Funcionario f : funcionarios) {
+            f.imprimirFuncionario();
+        }
+    }
 
-      }
+    public static boolean erroIDFuncionario(int idFunc) {
+        try {
+            for (Funcionario f : funcionarios) {
+                if (idFunc == f.getId()) {
+                    return true;
+                }
+            }
+        } catch (InputMismatchException e) {
+            System.err.println("Por favor, digite o ID do funcionário!");
+            input.nextLine();
+        }
+        return false;
+    }
 
-    public void excluirFuncionario(){
-        System.out.println("Digite o ID do funcionário:");
-        funcionario.setId(Integer.parseInt(input.nextLine()));
-        funcionarios.remove(funcionario);
-  }
+    public static void cadastrarFuncionario() {
+        Funcionario f = new Funcionario();
+        System.out.println("\nCadastramento de Funcionário\n");
+
+        f.setId(id);
+
+        preencherDados(f);
+
+        funcionarios.add(f);
+        id += 1;
+
+        System.out.println("\nFuncionário cadastrado!\n");
+        f.imprimirFuncionario();
+    }
+
+    public static void alterarFuncionario() {
+        if (funcionarios.size() <= 0) {
+            System.err.println("\nA empresa não possui candidatos válidos para a alteração!\n");
+        } else {
+            System.out.println("\nAlteração de dados\n");
+            listaFuncionarios();
+            System.out.println("Escolha o funcionário a ser alterado por ID: ");
+            int idFunc = input.nextInt();
+            if (erroIDFuncionario(idFunc)) {
+                for (Funcionario f : funcionarios) {
+                    if (idFunc == f.getId()) {
+                        preencherDados(f);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    public static void excluirFuncionario() {
+        if (funcionarios.size() <= 1) {
+            System.err.println("\nA empresa não possui candidatos válidos para a exclusão!\n");
+        } else {
+            listaFuncionarios();
+            System.out.println("Escolha o funcionário que deseja excluir por ID: ");
+            int idFunc = input.nextInt();
+            if (erroIDFuncionario(idFunc)) {
+                for (Funcionario f : funcionarios) {
+                    if (idFunc == f.getId()) {
+                        funcionarios.remove(f);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    public static void sair() {
+        System.out.println("Saindo do Sistema...");
+        System.exit(0);
+    }
 }
